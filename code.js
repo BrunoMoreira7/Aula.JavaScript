9622dd5eee9c4bcba10ce546ec41fd6f70c6de53
Bruno Moreira <92762737+BrunoMoreira7@users.noreply.github.com>
2021-11-08 18:07:05 -0300

    Delete code.js

--- code.js
+++
@@ -1,7 +0,0 @@
-function exibirTexto() {
-    document.getElementById("text").style.display = "block";
-}
-
-function ocultarTexto() {
-    document.getElementById("text").style.display = "none";
-}
